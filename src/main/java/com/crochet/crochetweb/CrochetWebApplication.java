47b07ba2c57a6994aea47bf305b1026be5aa472b
Rishika K <Rishi@MacBookAir.lan>
2026-03-17 15:31:33 -0500

    Initial clean commit - crochet website

---
+++ src/main/java/com/crochet/crochetweb/CrochetWebApplication.java
@@ -0,0 +1,13 @@
+package com.crochet.crochetweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrochetWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrochetWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crochet/crochetweb/CrochetWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crochet.crochetweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochetWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
